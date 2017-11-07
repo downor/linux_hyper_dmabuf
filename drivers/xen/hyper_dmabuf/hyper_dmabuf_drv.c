ee2aff21d12c8893e9cac9ddfa8547f9b1eadf5a
Kim, Dongwon <dongwon.kim@intel.com>
2017-11-07 13:31:38 -0800

    hyper_dmabuf: configure license
    Set the license of the driver to "GPL and MIT-X dual" and owner
    to "Intel". Also attached license term to all source and header
    files
    
    Signed-off-by: Kim, Dongwon <dongwon.kim@intel.com>

--- drivers/xen/hyper_dmabuf/hyper_dmabuf_drv.c
+++ drivers/xen/hyper_dmabuf/hyper_dmabuf_drv.c
@@ -1,3 +1,31 @@
+/*
+ * Copyright © 2017 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ * Authors:
+ *    Dongwon Kim <dongwon.kim@intel.com>
+ *    Mateusz Polrola <mateuszx.potrola@intel.com>
+ *
+ */
+
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/workqueue.h>
@@ -13,8 +41,8 @@
 extern struct hyper_dmabuf_backend_ops xen_backend_ops;
 #endif
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("IOTG-PED, INTEL");
+MODULE_LICENSE("GPL and additional rights");
+MODULE_AUTHOR("Intel Corporation");
 
 int register_device(void);
 int unregister_device(void);
